274d227472173ac095a119fa0e3072c005a889cf
Kian1973 <32907786+Kian1973@users.noreply.github.com>
2026-09-22 05:23:41 +0200

    Voeg service worker voor Vakento app toe

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = "vakento-app-v2";
+const SHELL = ["/app.html","/css/vakento.css","/css/vakento-app.css","/js/vakento-app.js","/js/api.js","/assets/vakento-app-icon.svg"];
+self.addEventListener("install", (e) => e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).catch(()=>{})));
+self.addEventListener("activate", (e) => e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k)=>k!==CACHE).map((k)=>caches.delete(k))))));
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET" || new URL(e.request.url).pathname.startsWith("/api/")) return;
+  e.respondWith(fetch(e.request).then((r) => { const copy=r.clone(); caches.open(CACHE).then((c)=>c.put(e.request,copy)); return r; }).catch(()=>caches.match(e.request)));
+});

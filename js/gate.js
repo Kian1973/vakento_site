b4a3cbd3634e9c59e5d378ca9ad786bb81cb25e2
Kian1973 <32907786+Kian1973@users.noreply.github.com>
2026-09-22 07:07:23 +0200

    Laad nieuwe btw-module

--- js/gate.js
+++ js/gate.js
@@ -17,7 +17,7 @@ try {
         "Gratis tot " + tot + ". Daarna stopt het vanzelf. Betaal je, dan blijft alles staan. <a href=\"account.html\">Betalen</a>";
       document.querySelector(".app-bar")?.insertAdjacentElement("afterend", note);
     }
-    await import("./app.js?v=full1");
+    await import("./app.js?v=btw1");
   }
 } catch (err) {
   window.VakentoHeal?.report?.(err, { kind: "gate-load" });
